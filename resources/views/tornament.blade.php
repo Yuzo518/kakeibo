5b65c7561ae1110e09db86a2db6f8c1bdfd20d90
Yuzo <hirakawa.yuzo@gmail.com>
2021-09-24 22:30:30 +0900

    fontawesomeの導入

--- resources/views/tornament.blade.php
+++ resources/views/tornament.blade.php
@@ -16,21 +16,21 @@
   <tr>
     <td scope="row">Test1</td>
     <td class="bg-light"></td>
-    <td>○<br>(2 - 1)</td>
-    <td>○<br>(2 - 0)</td>
+    <td><i class="far fa-circle text-success"></i><br>(2 - 1)</td>
+    <td>-<br>(0 - 0)</td>
     <td style="border-left:3px double green;">1位<br>(+3)</td>
   </tr>
   <tr>
     <td scope="row">Test2</td>
-    <td>×<br>(1 - 2)</td>
+    <td><i class="fas fa-times text-success"></i><br>(1 - 2)</td>
     <td class="bg-light"></td>
-    <td>○<br>(2 - 1)</td>
+    <td>-<br>(0 - 0)</td>
     <td style="border-left:3px double green;">2位<br>(+0)</td>
   </tr>
   <tr>
     <td scope="row">Test3</td>
-    <td>×<br>(0 - 2)</td>
-    <td>×<br>(1 - 2)</td>
+    <td>-<br>(0 - 0)</td>
+    <td>-<br>(0 - 0)</td>
     <td class="bg-light"></td>
     <td style="border-left:3px double green;">3位<br>(-3)</td>
   </tr>
@@ -57,33 +57,36 @@
           <input type="number" value="1" disabled>
         </td>
         <td><a href="#" class="btn btn-secondary" style="pointer-events:none;">試合終了</a></td>
-        <td><a href="#" class="btn btn-warning">内容修正</a></td>
+        <td><a href="#" class="btn btn-success">内容修正</a></td>
+        <td><a href="#" class="btn btn-secondary" style="pointer-events:none;">無効試合</a></td>
       </tr>
       <tr>
         <td>2</td>
         <td>
           Test1<br>
-          <input type="number">
+          <input type="number" value="0">
         </td>
         <td>
           Test3<br>
-          <input type="number">
+          <input type="number" value="0">
         </td>
         <td><a href="#" class="btn btn-primary">試合終了</a></td>
         <td><a href="#" class="btn btn-secondary" style="pointer-events:none;">内容修正</a></td>
+        <td><a href="#" class="btn btn-warning">無効試合</a></td>
       </tr>
       <tr>
         <td>3</td>
         <td>
           Test2<br>
-          <input type="number">
+          <input type="number" value="0">
         </td>
         <td>
           Test3<br>
-          <input type="number">
+          <input type="number" value="0">
         </td>
         <td><a href="#" class="btn btn-primary">試合終了</a></td>
         <td><a href="#" class="btn btn-secondary" style="pointer-events:none;">内容修正</a></td>
+        <td><a href="#" class="btn btn-warning">無効試合</a></td>
       </tr>
     </tbody>
   </table>
